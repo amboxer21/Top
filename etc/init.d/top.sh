720982641b4ff8adace5b5086192ae912be9b1aa
Anthony Guevara <amboxer21@gmail.com>
2017-08-22 12:28:59 -0400

    Update top.sh

--- etc/init.d/top.sh
+++ etc/init.d/top.sh
@@ -3,8 +3,4 @@
 # chkconfig: - 10 90
 # description: blah 
 
-# Add to runtime with chkconfig --add top.sh
-# run with bash /etc/init.d/top.sh
-# turn the script on now with chkconfig top.sh on
-
 screen -dmS name watch -n1 /usr/local/bin/ruby /home/anthony/Documents/Ruby/top.rb 60 8
